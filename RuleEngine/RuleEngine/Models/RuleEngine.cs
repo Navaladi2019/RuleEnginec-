40cc089cfc37328274a171436e4f68ad65ad8d03
naval <vknavaladi@gmail.com>
2024-10-19 13:36:51 +0530

    rule engine model

---
+++ RuleEngine/RuleEngine/Models/RuleEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleEngine.Models
+{
+    public class RuleEngine(dynamic Inputparam, ITGRuleSet RulesetParam)
+    {
+        public  dynamic Input { get => Inputparam; }
+        public  ITGRuleSet RuleSet { get => RulesetParam; }
+        public required Dictionary<string, dynamic> Ctx { get; set; } = [];
+    }
+}
